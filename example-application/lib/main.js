39477f0ca87bb83e5da3b29487ea732a6cfb2dd6
Jannik Jochem <jannik.jochem@pickware.com>
2019-05-30 12:42:08 +0200

    Ask the user for their name

--- example-application/lib/main.js
+++ example-application/lib/main.js
@@ -1,13 +1,38 @@
 const adjectiveAnimal = require('adjective-animal');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
-
 const serverName = adjectiveAnimal.generateName();
 
-app.get('/', (req, res) => res.send(`
-<h1>Hello World from ${serverName}!</h1>
-`));
+app.use(bodyParser.urlencoded({extended: true}));
+
+let indexTemplate = name => `
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>${serverName}</title>
+</head>
+
+<h1>
+${name ? `Hello ${name} from ${serverName}!` : `Hello World from ${serverName}!`}
+</h1>
+    
+<form method="post">
+    <label>
+        What's your name?
+        <input type="text" name="name"/>
+    </label>
+    <input type="submit"/>
+</form>
+
+</html>
+`;
+
+app.get('/', (req, res) => res.send(indexTemplate()));
+
+app.post('/', (req, res) => res.send(indexTemplate(req.body.name)));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
