2954affc9af19f21df6fa0beb2bae1d5d2c54222
Jannik Jochem <jannik.jochem@pickware.com>
2019-05-30 12:18:46 +0200

    Add server node identifier

--- example-application/lib/main.js
+++ example-application/lib/main.js
@@ -1,8 +1,13 @@
+const adjectiveAnimal = require('adjective-animal');
 const express = require('express');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const serverName = adjectiveAnimal.generateName();
+
+app.get('/', (req, res) => res.send(`
+<h1>Hello World from ${serverName}!</h1>
+`));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
