d54f27bb52d22e1af9f5f96e7100efcd79a2e5ce
Jannik Jochem <jannik.jochem@pickware.com>
2019-06-21 10:14:37 +0200

    Fix path construction

--- control-application/lib/routes/create-deployment.js
+++ control-application/lib/routes/create-deployment.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const util = require('util');
 const k8s = require('@kubernetes/client-node');
 
@@ -12,14 +13,14 @@ module.exports = async () => {
     const k8sCoreApi = kc.makeApiClient(k8s.CoreV1Api);
 
     // Create deployment
-    const deployment = k8s.loadYaml(await readFile('../kubernetes/deployment.yml'));
+    const deployment = k8s.loadYaml(await readFile(path.join(__dirname, '../../../kubernetes/deployment.yml')));
     await k8sExtensionsApi.createNamespacedDeployment('default', deployment);
 
     // Create ingress
-    const ingress = k8s.loadYaml(await readFile('../kubernetes/ingress.yml'));
+    const ingress = k8s.loadYaml(await readFile(path.join(__dirname, '../../../kubernetes/ingress.yml')));
     await k8sExtensionsApi.createNamespacedIngress('default', ingress);
 
     // Create Service
-    const service = k8s.loadYaml(await readFile('../kubernetes/service.yml'));
+    const service = k8s.loadYaml(await readFile(path.join(__dirname, '../../../kubernetes/service.yml')));
     await k8sCoreApi.createNamespacedService('default', service);
 };
