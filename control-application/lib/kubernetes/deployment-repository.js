3fd57435b594d25d84231ac98c622d174aed2919
Niklas Büchner <niklas.buechner@viison.com>
2019-06-23 16:53:15 +0200

    Add scale function to deployment repository

--- control-application/lib/kubernetes/deployment-repository.js
+++ control-application/lib/kubernetes/deployment-repository.js
@@ -5,4 +5,20 @@ const namespace = 'default';
 module.exports = {
     create: async deployment => apiClient.createNamespacedDeployment(namespace, deployment.getSpec()),
     delete: async deploymentName => apiClient.deleteNamespacedDeployment(deploymentName, namespace),
+    scale: async (deploymentName, scale) => apiClient.patchNamespacedDeployment(
+        deploymentName,
+        namespace,
+        {
+            spec: {
+                replicas: scale,
+            },
+        },
+        undefined,
+        undefined,
+        {
+            headers: {
+                'Content-Type': 'application/strategic-merge-patch+json',
+            },
+        }
+    ),
 };
